c900fe81a2b546884455d8b2df1a63857563e44c
Omega2418 <h.oliver06@gmail.com>
2024-11-04 01:14:53 +0100

    footer.jsx fixed

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from '@inertiajs/react';
 
-import '../../css/Footer.css'
 
-function Footer () {
-
-    return (
-      <footer className="footer">
-        <p>
-          <Link>About Us</Link> <span className="dot"></span> 
-          <Link>Privacy Policy</Link> <span className="dot"></span> 
-          ©2024 Inkli
-        </p>
-
-      </footer>
-    )
+function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white   
+ py-4 text-center border-t border-gray-700">
+      <p>
+        <Link to="/about" className="hover:underline">About Us</Link> <span className="inline-block w-2 h-2 rounded-full bg-white mx-2 mb-1"></span> 
+        <Link to="/privacy-policy" className="hover:underline">Privacy Policy</Link> <span className="inline-block w-2 h-2 rounded-full bg-white mx-2 mb-1"></span> 
+        ©2024 Inkli
+      </p>
+    </footer>
+  );
 }
-
 
 export default Footer;
