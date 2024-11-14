d803dcc259f7f6dcdef0c312dee31ec7158f24dd
Krismics1 <krisidienes01@gmail.com>
2024-11-14 16:43:52 +0100

    modified footer to link to the aboutus

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import { Link } from '@inertiajs/react';
-
+import React from "react";
+import { Link } from "@inertiajs/react";
 
 function Footer() {
-  return (
-    <footer className="bg-[#171717] text-white   
- py-4 text-center border-t border-gray-800">
-      <p>
-        <Link to="/about" className="hover:underline">About Us</Link> <span className="inline-block w-2 h-2 rounded-full bg-white mx-2 mb-1"></span> 
-        <Link to="/privacy-policy" className="hover:underline">Privacy Policy</Link> <span className="inline-block w-2 h-2 rounded-full bg-white mx-2 mb-1"></span> 
-        ©2024 Inkli
-      </p>
-    </footer>
-  );
+    return (
+        <footer
+            className="bg-[#171717] text-white   
+ py-4 text-center border-t border-gray-800"
+        >
+            <p>
+                <Link href="/aboutus" className="hover:underline">
+                    About Us
+                </Link>{" "}
+                <span className="inline-block w-2 h-2 rounded-full bg-white mx-2 mb-1"></span>
+                <Link to="/privacy-policy" className="hover:underline">
+                    Privacy Policy
+                </Link>{" "}
+                <span className="inline-block w-2 h-2 rounded-full bg-white mx-2 mb-1"></span>
+                ©2024 Inkli
+            </p>
+        </footer>
+    );
 }
 
 export default Footer;
