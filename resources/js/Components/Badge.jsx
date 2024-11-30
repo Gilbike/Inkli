f3f44c8b0fa98f8af8e8ed64515144e83c606843
Krismics1 <krisidienes01@gmail.com>
2024-11-30 18:12:25 +0100

    Made badge component

---
+++ resources/js/Components/Badge.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Badge({ img, title }) {
+    return (
+        <div class="m-2 w-20  text-center ">
+            <div className="p-2">
+                <img className="rounded-lg" src={img} alt={title} />
+            </div>
+            <p className="text-xs">{title}</p>
+        </div>
+    );
+}
