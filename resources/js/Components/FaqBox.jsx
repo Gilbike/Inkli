3b62d8824d346d86b3e4047a3af6cb6db7ceed11
Krismics1 <krisidienes01@gmail.com>
2024-11-30 18:12:57 +0100

    Fixed faqbox bg color

--- resources/js/Components/FaqBox.jsx
+++ resources/js/Components/FaqBox.jsx
@@ -7,8 +7,10 @@ export default function FaqBox({ question, answer }) {
     return (
         <div className="w-full rounded-lg mb-2 overflow-hidden">
             <p
-                className={`font-medium text-2xl p-3 hover:bg-dark1-h cursor-pointer flex flex-row justify-between items-center ${
-                    expanded ? "bg-dark1-c" : "bg-dark1"
+                className={`font-medium text-2xl p-3 dark:hover:bg-dark1-h hover:bg-light1-h cursor-pointer flex flex-row justify-between items-center ${
+                    expanded
+                        ? "dark:bg-dark1-c bg-light1-c"
+                        : "dark:bg-dark1 bg-light1"
                 }`}
                 onClick={() => setExpanded(!expanded)}
             >
