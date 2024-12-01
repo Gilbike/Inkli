b81a946540ec79e92d8c0313fedfde59259c4be9
Krismics1 <krisidienes01@gmail.com>
2024-12-01 14:38:21 +0100

    Fixed button background

--- resources/js/Components/Button.jsx
+++ resources/js/Components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({
     children,
     big = null,
     click,
-    className,
+    className = "dark:bg-darkP bg-lightP",
     ...props
 }) {
     let size = big ? "py-3 px-4" : "py-2 px-3";
@@ -13,7 +13,7 @@ export default function Button({
         <button
             onClick={click}
             {...props}
-            className={`bg-lightP text-white  dark:bg-darkP hover:bg-lightH dark:hover:bg-darkH active:bg-lightA dark:active:bg-darkA  ${size} rounded-lg ${className}`}
+            className={` text-white   hover:bg-lightH dark:hover:bg-darkH active:bg-lightA dark:active:bg-darkA  ${size} rounded-lg ${" "} ${className}`}
         >
             {children}
         </button>
