19d9babc890d0a9025c02238465a66b4202a7b67
Krismics1 <krisidienes01@gmail.com>
2024-12-09 16:43:08 +0100

    header buttons logged in

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 import { IoMdAdd, IoMdNotifications, IoMdSearch } from "react-icons/io";
 import Input from "./Input";
 
 function Header() {
+    const { auth } = usePage().props;
+    // auth.user = null;
     return (
         <nav className="dark:bg-dark1 bg-light1 p-3 flex justify-between items-center z-50 border-b border-b-outlineColor relative">
             <Link
-                href="/"
+                href={auth.user ? "/stories" : "/"}
                 className="text-2xl font-bold flex flex-row items-center w-10 gap-2"
             >
                 <img
@@ -17,6 +19,7 @@ function Header() {
                 />
                 Inkli
             </Link>
+
             <div className="absolute left-0 w-full h-full flex items-center justify-center z-40 pointer-events-none">
                 <Input
                     cl
@@ -25,27 +28,34 @@ function Header() {
                     className="w-4/12 pointer-events-auto bg-light3-h "
                 />
             </div>
-            <div className="flex flex-row gap-2">
-                <Link
-                    href="/create"
-                    className="flex flex-row gap-3 items-center p-2 rounded-md dark:bg-dark2 dark:hover:bg-dark2-h bg-light2 hover:bg-light2-h"
-                >
-                    Create
-                    <IoMdAdd size={24} />
+
+            {auth.user ? (
+                <div className="flex flex-row gap-2">
+                    <Link
+                        href="/create"
+                        className="flex flex-row gap-3 items-center p-2 rounded-md dark:bg-dark2 dark:hover:bg-dark2-h bg-light2 hover:bg-light2-h"
+                    >
+                        Create
+                        <IoMdAdd size={24} />
+                    </Link>
+                    <Link
+                        href="/notifications"
+                        className="flex items-center p-2 rounded-md dark:bg-dark2 dark:hover:bg-dark2-h bg-light2 hover:bg-light2-h"
+                    >
+                        <IoMdNotifications size={24} />
+                    </Link>
+                    <Link
+                        href="/profile"
+                        className="p-2 rounded-md dark:bg-dark2 dark:hover:bg-dark2-h bg-light2 hover:bg-light2-h"
+                    >
+                        {auth.user.name}
+                    </Link>
+                </div>
+            ) : (
+                <Link className="mr-2" href="/login">
+                    Already a member?
                 </Link>
-                <Link
-                    href="/notifications"
-                    className="flex items-center p-2 rounded-md dark:bg-dark2 dark:hover:bg-dark2-h bg-light2 hover:bg-light2-h"
-                >
-                    <IoMdNotifications size={24} />
-                </Link>
-                <Link
-                    href="/profile"
-                    className="p-2 rounded-md dark:bg-dark2 dark:hover:bg-dark2-h bg-light2 hover:bg-light2-h"
-                >
-                    Name
-                </Link>
-            </div>
+            )}
         </nav>
     );
 }
