a7727ce6e0ec674a7bb6c7bceb7df75b10c3b19a
Gilbike <marton.dikacz@gmail.com>
2024-12-17 13:48:50 +0100

    add responsiveness to profile page

--- resources/js/Components/Container.jsx
+++ resources/js/Components/Container.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Container({ children }) {
-    return <div className="w-7/12 mx-auto">{children}</div>;
+    return (
+        <div className="w-11/12 md:w-9/12 lg:w-7/12 mx-auto">{children}</div>
+    );
 }
--- resources/js/Components/ProfileCard.jsx
+++ resources/js/Components/ProfileCard.jsx
@@ -11,10 +11,10 @@ export default function ProfileCard({ name, pfp, wrote, subm, acc }) {
                     src={pfp ?? "/img/default-pfp.jpg"}
                     alt={name}
                 />
-                <div className="flex justify-between w-full m-2 ml-7">
+                <div className="flex flex-col md:flex-row justify-between w-full m-2 ml-7">
                     <div className="flex flex-col justify-between h-full">
                         <h1 className="block text-5xl font-bold">{name}</h1>
-                        <p className="text-sm block ">
+                        <p className="text-sm block">
                             <b>{wrote}</b> stories written <br />
                             <b>{subm}</b> continues submitted <br />
                             <b>{acc}</b> continues accepted <br />
@@ -22,11 +22,7 @@ export default function ProfileCard({ name, pfp, wrote, subm, acc }) {
                     </div>
                     <div className="flex flex-col w-28">
                         <Button>Follow</Button>
-                        <Button
-                            className={
-                                " bg-secondaryP dark:bg-secondaryP dark:hover:bg-secondaryH hover:bg-secondaryH dark:active:bg-secondaryA active:bg-secondaryA w-full mt-1.5"
-                            }
-                        >
+                        <Button className="bg-secondaryP dark:bg-secondaryP dark:hover:bg-secondaryH hover:bg-secondaryH dark:active:bg-secondaryA active:bg-secondaryA w-full mt-1.5">
                             Block
                         </Button>
                     </div>
