befa09ff2581284e6b2d2a258374f0f361403701
Gilbike <marton.dikacz@gmail.com>
2025-01-30 14:34:19 +0100

    add header dropdown, logout

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, usePage } from "@inertiajs/react";
 import { IoMdAdd, IoMdNotifications, IoMdSearch } from "react-icons/io";
 import { IoInvertMode } from "react-icons/io5";
@@ -7,6 +7,9 @@ import Input from "./Input";
 function Header({ onDarkModeClick }) {
     const { auth } = usePage().props;
     // auth.user = null;
+
+    const [open, setOpen] = useState(false);
+
     return (
         <nav className="dark:bg-dark1 bg-light1 p-3 flex justify-between items-center z-50 border-b border-b-outlineColor relative">
             <Link
@@ -24,7 +27,6 @@ function Header({ onDarkModeClick }) {
             {auth.user && (
                 <div className="absolute left-0 w-full h-full flex items-center justify-center z-40 pointer-events-none">
                     <Input
-                        cl
                         svg={<IoMdSearch />}
                         placeholder="Search"
                         className="w-4/12 pointer-events-auto bg-light3-h "
@@ -47,25 +49,53 @@ function Header({ onDarkModeClick }) {
                     >
                         <IoMdNotifications size={24} />
                     </Link>
-                    <button
-                        className="flex items-center py-2 px-3 rounded-md dark:bg-dark2 dark:hover:bg-dark2-h bg-light2 hover:bg-light2-h"
-                        onClick={onDarkModeClick}
-                    >
-                        <IoInvertMode />
-                    </button>
-                    <Link
-                        href="/profile"
-                        className="p-2 rounded-md dark:bg-dark2 dark:hover:bg-dark2-h bg-light2 hover:bg-light2-h flex flex-row gap-3"
-                    >
-                        {auth.user.name}
-                        <img
-                            className="rounded w-6 h-6"
-                            src={
-                                auth.user.profilepicture ??
-                                "/img/default-pfp.jpg"
-                            }
-                        />
-                    </Link>
+                    <div className="relative">
+                        <button
+                            className="p-2 rounded-md dark:bg-dark2 dark:hover:bg-dark2-h bg-light2 hover:bg-light2-h flex flex-row gap-3"
+                            onClick={() => setOpen(!open)}
+                        >
+                            {auth.user.name}
+                            <img
+                                className="rounded w-6 h-6"
+                                src={
+                                    auth.user.profilepicture ??
+                                    "/img/default-pfp.jpg"
+                                }
+                            />
+                        </button>
+
+                        {open && (
+                            <div
+                                className="fixed inset-0 z-40"
+                                onClick={() => setOpen(false)}
+                            ></div>
+                        )}
+
+                        {open ? (
+                            <div className="absolute w-48 right-0 dark:bg-dark2 bg-light2 shadow rounded-lg overflow-hidden z-50">
+                                <Link
+                                    className="w-full flex flex-row items-center gap-2 py-2 px-3 rounded-md  dark:hover:bg-dark2-h  hover:bg-light2-h"
+                                    href="/profile"
+                                >
+                                    Profile
+                                </Link>
+                                <button
+                                    className="w-full flex flex-row items-center gap-2 py-2 px-3 rounded-md  dark:hover:bg-dark2-h  hover:bg-light2-h"
+                                    onClick={onDarkModeClick}
+                                >
+                                    <IoInvertMode />
+                                    Toggle dark theme
+                                </button>
+                                <Link
+                                    href="/logout"
+                                    method="POST"
+                                    className="w-full flex flex-row items-center gap-2 py-2 px-3 rounded-md  dark:hover:bg-dark2-h  hover:bg-light2-h"
+                                >
+                                    Logout
+                                </Link>
+                            </div>
+                        ) : null}
+                    </div>
                 </div>
             ) : (
                 <Link className="mr-2" href="/login">
