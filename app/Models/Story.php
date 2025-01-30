8c785ca43630658677a10a2d956f76da841cbf2d
Gilbike <marton.dikacz@gmail.com>
2025-01-30 13:53:00 +0100

    add story factory and seeder

--- app/Models/Story.php
+++ app/Models/Story.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Story extends Model
 {
+    use HasFactory;
+
     public function author()
     {
         return $this->belongsTo(User::class, "author");
