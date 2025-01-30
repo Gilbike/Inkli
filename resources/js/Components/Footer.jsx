3d6d96c20c7924c75ed596ac4b029b6994be7455
Gilbike <marton.dikacz@gmail.com>
2025-01-30 11:05:11 +0100

    fix dark mode for footer

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -3,16 +3,16 @@ import { Link } from "@inertiajs/react";
 
 function Footer() {
     return (
-        <footer className="bg-dark1 text-white py-4 text-center border-t border-outlineColor">
+        <footer className="bg-light1 dark:bg-dark1 dark:text-darkT text-lightT py-4 text-center border-t border-outlineColor">
             <p>
                 <Link href="/aboutus" className="hover:underline">
                     About Us
                 </Link>{" "}
-                <span className="inline-block w-2 h-2 rounded-full bg-white mx-2 mb-1"></span>
+                <span className="inline-block w-2 h-2 rounded-full bg-black dark:bg-white mx-2 mb-1"></span>
                 <Link to="/privacy-policy" className="hover:underline">
                     Privacy Policy
                 </Link>{" "}
-                <span className="inline-block w-2 h-2 rounded-full bg-white mx-2 mb-1"></span>
+                <span className="inline-block w-2 h-2 rounded-full bg-black dark:bg-white mx-2 mb-1"></span>
                 ©2024 Inkli
             </p>
         </footer>
