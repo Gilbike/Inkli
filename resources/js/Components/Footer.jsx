ab8c5ad0793b024a2ae24ac3174167ae3be23520
Gilbike <marton.dikacz@gmail.com>
2025-02-22 18:48:57 +0100

    fix footer, fix title

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -3,18 +3,16 @@ import { Link } from "@inertiajs/react";
 
 function Footer() {
     return (
-        <footer className="bg-light1 dark:bg-dark1 dark:text-darkT text-lightT py-4 text-center border-t border-outlineColor">
-            <p>
-                <Link href="/aboutus" className="hover:underline">
-                    About Us
-                </Link>{" "}
-                <span className="inline-block w-2 h-2 rounded-full bg-black dark:bg-white mx-2 mb-1"></span>
-                <Link to="/privacy-policy" className="hover:underline">
-                    Privacy Policy
-                </Link>{" "}
-                <span className="inline-block w-2 h-2 rounded-full bg-black dark:bg-white mx-2 mb-1"></span>
-                ©2024 Inkli
-            </p>
+        <footer className="bg-light1 dark:bg-dark1 dark:text-darkT text-lightT py-4 border-t border-outlineColor flex flex-row gap-2 items-center justify-center">
+            <Link href="/aboutus" className="hover:underline">
+                About Us
+            </Link>
+            <span>•</span>
+            <Link to="/privacy-policy" className="hover:underline">
+                Privacy Policy
+            </Link>
+            <span>•</span>
+            ©2024 Inkli
         </footer>
     );
 }
