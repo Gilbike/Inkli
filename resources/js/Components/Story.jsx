afa91b9680339dc9c5f3dc26398c9208ec0eb24e
Krismics1 <krisidienes01@gmail.com>
2025-03-13 22:14:29 +0100

    fetch backend for story.jsx

--- resources/js/Components/Story.jsx
+++ resources/js/Components/Story.jsx
@@ -4,6 +4,8 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 
 export default function Story({ id, title, summary, likeCount }) {
     const [shared, setShared] = useState(false);
+    const [rated, setRated] = useState(false);
+    const [currentLikeCount, setCurrentLikeCount] = useState(likeCount);
 
     const shareStory = () => {
         navigator.clipboard.writeText(route("stories.show", { story: id }));
@@ -11,11 +13,34 @@ export default function Story({ id, title, summary, likeCount }) {
     };
 
     function handleLike(like) {
-        if (like) {
-            console.log("Liked");
-        } else {
-            console.log("Disliked");
-        }
+        if (rated) return;
+        const csrfToken = document
+            .querySelector('meta[name="csrf-token"]')
+            ?.getAttribute("content");
+
+        fetch(
+            route(like ? "stories.like" : "stories.dislike", {
+                story: id /* ide be kéne adni a like változót és akkor nem kell az egy soros if */,
+            }),
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": csrfToken,
+                },
+                body: JSON.stringify({ like }),
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    setCurrentLikeCount(data.likeCount);
+                    setRated(true);
+                } else {
+                    console.error(data.message);
+                }
+            })
+            .catch((error) => console.error(error));
     }
 
     useEffect(() => {
@@ -25,7 +50,6 @@ export default function Story({ id, title, summary, likeCount }) {
                 setShared(false);
             }, 1500);
         }
-
         return () => clearTimeout(timer);
     }, [shared]);
 
@@ -38,20 +62,21 @@ export default function Story({ id, title, summary, likeCount }) {
             <div className="flex flex-row gap-2">
                 <div className="flex flex-row gap-2 dark:bg-dark2 bg-light2 items-center">
                     <button
+                        disabled={rated}
                         onClick={() => handleLike(true)}
                         className="p-2 dark:bg-dark3 bg-light3 rounded flex items-center"
                     >
                         <IoMdThumbsUp className="w-4 h-4" />
                     </button>
-                    <p className="text-xs">{likeCount}</p>
+                    <p className="text-xs">{currentLikeCount}</p>
                     <button
+                        disabled={rated}
                         onClick={() => handleLike(false)}
                         className="p-2 dark:bg-dark3 bg-light3 rounded flex items-center"
                     >
                         <IoMdThumbsDown className="w-4 h-4" />
                     </button>
                 </div>
-
                 <button
                     onClick={shareStory}
                     className="p-2 dark:bg-dark2 bg-light2 rounded flex flex-row items-center gap-2 px-3"
@@ -59,7 +84,6 @@ export default function Story({ id, title, summary, likeCount }) {
                     <p className="text-xs">Share</p>
                     <FaExternalLinkAlt className="w-4 h-4" />
                 </button>
-
                 {shared && (
                     <div className="rounded text-xs bg-lightH/40 flex items-center px-2 dark:bg-darkH text-stone-900 dark:text-neutral-300">
                         Link copied to clipboard
