2ca6a5624d405d6446e90ba148dfbf726397cab4
Gilbike <marton.dikacz@gmail.com>
2025-03-13 13:57:42 +0100

    add story share button

--- resources/js/Components/Story.jsx
+++ resources/js/Components/Story.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IoMdThumbsUp, IoMdThumbsDown } from "react-icons/io";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
 export default function Story({ id, title, summary, likeCount }) {
+    const [shared, setShared] = useState(false);
+
+    const shareStory = () => {
+        navigator.clipboard.writeText(route("stories.show", { story: id }));
+        setShared(true);
+    };
+
+    useEffect(() => {
+        let timer;
+        if (shared) {
+            timer = setTimeout(() => {
+                setShared(false);
+            }, 1500);
+        }
+
+        return () => clearTimeout(timer);
+    }, [shared]);
+
     return (
         <div className="p-4 rounded-lg dark:bg-dark1 bg-light1 flex flex-col gap-2">
-            {" "}
             <a href={route("stories.show", { story: id })}>
                 <h2 className="font-medium text-2xl line-clamp-1">{title}</h2>
                 <p className="font-[Playfair] line-clamp-2">{summary}</p>
@@ -33,10 +50,19 @@ export default function Story({ id, title, summary, likeCount }) {
                     </label>
                 </div>
 
-                <button className="p-2 dark:bg-dark2 bg-light2 rounded flex flex-row items-center gap-2 px-3">
+                <button
+                    onClick={shareStory}
+                    className="p-2 dark:bg-dark2 bg-light2 rounded flex flex-row items-center gap-2 px-3"
+                >
                     <p className="text-xs">Share</p>
                     <FaExternalLinkAlt className="w-4 h-4" />
                 </button>
+
+                {shared && (
+                    <div className="rounded text-xs bg-lightH/40 flex items-center px-2 text-neutral-300">
+                        Link copied to clipboard
+                    </div>
+                )}
             </div>
         </div>
     );
