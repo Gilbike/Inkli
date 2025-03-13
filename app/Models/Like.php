1f54d6fea03fcce641dbf7c0b89482a0782ae05b
Krismics1 <krisidienes01@gmail.com>
2025-03-13 22:08:09 +0100

    added like model belongings

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -7,5 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    use HasFactory;
+  use HasFactory;
+  public function story()
+  {
+    return $this->belongsTo(Story::class, 'story_id');
+  }
+  public function user()
+  {
+    return $this->belongsTo(User::class, 'user_id');
+  }
+  protected $fillable = ['user_id', 'story_id', 'is_liked'];
 }
