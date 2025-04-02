46dd9ca7ebce9e333475d8f07de6f91b409a8f2b
Gilbike <marton.dikacz@gmail.com>
2025-04-02 09:57:37 +0200

    add badges calculation, text

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,8 +15,50 @@ class ProfileController extends Controller
   {
     $user = auth()->user();
     $stories = $user->stories()->get();
+    $likes = $user->likes()->count();
 
-    return inertia("Profile", ["stories" => $stories]);
+    $badges = [
+      "stry" => null,
+      "like" => null,
+      "join" => null,
+      "rgst" => null
+    ];
+
+    if ($stories->count() >= 10) {
+      $badges["stry"] = "brnz";
+    } else if ($stories->count() >= 50) {
+      $badges["stry"] = "slvr";
+    } else if ($stories->count() >= 100) {
+      $badges["stry"] = "gld";
+    }
+
+    if ($likes >= 50) {
+      $badges["like"] = "brnz";
+    } else if ($likes >= 100) {
+      $badges["like"] = "slvr";
+    } else if ($likes >= 500) {
+      $badges["like"] = "gld";
+    }
+
+    // account age in months
+    $acc_age = date_diff(date_create($user->created_at), date_create("now"))->m;
+    if ($acc_age >= 0) {
+      $badges["join"] = "brnz";
+    } else if ($acc_age >= 6) {
+      $badges["join"] = "slvr";
+    } else if ($acc_age >= 12) {
+      $badges["join"] = "gld";
+    }
+
+    if ($user->id <= 1000) {
+      $badges["rgst"] = "brnz";
+    } else if ($user->id <= 100) {
+      $badges["rgst"] = "slvr";
+    } else if ($user->id <= 10) {
+      $badges["rgst"] = "gld";
+    }
+
+    return inertia("Profile", ["stories" => $stories, "badges" => $badges]);
   }
 
   /**
@@ -74,7 +116,49 @@ class ProfileController extends Controller
   public function other(User $user)
   {
     $stories = $user->stories()->get();
+    $likes = $user->likes()->count();
 
-    return inertia("Profile", ["user" => $user, "stories" => $stories]);
+    $badges = [
+      "stry" => null,
+      "like" => null,
+      "join" => null,
+      "rgst" => null
+    ];
+
+    if ($stories->count() >= 10) {
+      $badges["stry"] = "brnz";
+    } else if ($stories->count() >= 50) {
+      $badges["stry"] = "slvr";
+    } else if ($stories->count() >= 100) {
+      $badges["stry"] = "gld";
+    }
+
+    if ($likes >= 50) {
+      $badges["like"] = "brnz";
+    } else if ($likes >= 100) {
+      $badges["like"] = "slvr";
+    } else if ($likes >= 500) {
+      $badges["like"] = "gld";
+    }
+
+    // account age in months
+    $acc_age = date_diff(date_create($user->created_at), date_create("now"))->m;
+    if ($acc_age >= 0) {
+      $badges["join"] = "brnz";
+    } else if ($acc_age >= 6) {
+      $badges["join"] = "slvr";
+    } else if ($acc_age >= 12) {
+      $badges["join"] = "gld";
+    }
+
+    if ($user->id <= 10) {
+      $badges["rgst"] = "gld";
+    } else if ($user->id <= 100) {
+      $badges["rgst"] = "slvr";
+    } else if ($user->id <= 1000) {
+      $badges["rgst"] = "brnz";
+    }
+
+    return inertia("Profile", ["user" => $user, "stories" => $stories, "badges" => $badges]);
   }
 }
