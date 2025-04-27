1bd1a432c9a94ffe11e255490da69a411cd375ea
Krismics1 <krisidienes01@gmail.com>
2025-04-27 18:38:16 +0200

    removed unnecesary tests

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -2,27 +2,9 @@
 
 use App\Models\User;
 use App\Models\Genre;
-use App\Models\Story;
 
 
-it('checks if there is data in the users table', function () {
-  $this->assertTrue(User::exists(), 'The users table is empty.');
-});
-it('checks if there is data in the genres table', function () {
-  $this->assertTrue(Genre::exists(), 'The genres table is empty.');
-});
-it('checks if there is data in the stories table', function () {
-  $this->assertTrue(Story::exists(), 'The stories table is empty.');
-});
-it('checks if data can be added to the stories table', function () {
-  $story = Story::create([
-    'title'   => 'Test Story',
-    'content' => 'This is a test story content.',
-    'genre'   => 'Test Genre',
-    'author'  => 0, // Assuming 0 is the ID of the author in the users table
-  ]);
-  $this->assertTrue(Story::where('title', 'Test Story')->exists(), 'Failed to add data to the stories table.');
-});
+
 
 
 
